e660763141fe15ba4fd937bb534b03d1b3e82488
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-02-26 14:30:13 +0100

    fixed negative timer

--- js/script.js
+++ js/script.js
@@ -44,11 +44,20 @@ window.addEventListener("DOMContentLoaded", () => {
    const deadLine = "2025-05-20";
 
    function getTimeRemaining(endtiem) {
-      const total = Date.parse(endtiem) - Date.parse(new Date()),
-         days = Math.floor(total / (1000 * 60 * 60 * 24)),
-         hours = Math.floor((total / (1000 * 60 * 60)) % 24),
-         minutes = Math.floor((total / (1000 * 60)) % 60),
-         seconds = Math.floor((total / 1000) % 60);
+      let days, hours, minutes, seconds;
+      const total = Date.parse(endtiem) - Date.parse(new Date());
+
+      if (total <= 0) {
+         days = 0;
+         hours = 0;
+         minutes = 0;
+         seconds = 0;
+      } else {
+         (days = Math.floor(total / (1000 * 60 * 60 * 24))),
+            (hours = Math.floor((total / (1000 * 60 * 60)) % 24)),
+            (minutes = Math.floor((total / (1000 * 60)) % 60)),
+            (seconds = Math.floor((total / 1000) % 60));
+      }
 
       return {
          total,
