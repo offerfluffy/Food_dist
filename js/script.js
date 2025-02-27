96f465ec254c15cbf9707c170045bb7139da2254
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-02-27 18:11:14 +0100

    added class variation of creating Menu

--- js/script.js
+++ js/script.js
@@ -107,7 +107,7 @@ window.addEventListener("DOMContentLoaded", () => {
       modalCloseBtn = document.querySelector("[data-close]"),
       modal = document.querySelector(".modal");
 
-   const modalTimerId = setTimeout(showModal, 10000);
+   // const modalTimerId = setTimeout(showModal, 10000);
 
    modalTriggers.forEach((modalTrigger) => {
       modalTrigger.addEventListener("click", showModal);
@@ -152,4 +152,70 @@ window.addEventListener("DOMContentLoaded", () => {
          window.removeEventListener("scroll", showModalByScroll);
       }
    }
+
+   // Menu field
+
+   const menuField = document.querySelector(".menu__field .container");
+
+   function clearItem(item) {
+      item.innerHTML = "";
+   }
+   class MenuItem {
+      constructor(imageSrc, imageALt, subtitle, description, price) {
+         this.imageSrc = imageSrc;
+         this.imageALt = imageALt;
+         this.subtitle = subtitle;
+         this.description = description;
+         this.price = price;
+      }
+
+      render(parent) {
+         parent.innerHTML += `
+            <div class="menu__item">
+                  <img src="${this.imageSrc}" alt="${this.imageALt}" />
+                  <h3 class="menu__item-subtitle">Меню "${this.subtitle}"</h3>
+                  <div class="menu__item-descr">
+                     ${this.description}
+                  </div>
+                  <div class="menu__item-divider"></div>
+                  <div class="menu__item-price">
+                     <div class="menu__item-cost">Цена:</div>
+                     <div class="menu__item-total">
+                        <span>${+this.price}</span> грн/день
+                     </div>
+                  </div>
+            </div>
+         `;
+      }
+   }
+
+   const fitnesMenuItem = new MenuItem(
+      "./img/tabs/vegy.jpg",
+      "vegy",
+      "Фитнес",
+      `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+      229
+   );
+
+   const premiumMenuItem = new MenuItem(
+      "img/tabs/elite.jpg",
+      "elite",
+      "Премиум",
+      `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
+      550
+   );
+
+   const postMenuItem = new MenuItem(
+      "img/tabs/post.jpg",
+      "post",
+      "Постное",
+      `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
+      430
+   );
+
+   clearItem(menuField);
+
+   fitnesMenuItem.render(menuField);
+   premiumMenuItem.render(menuField);
+   postMenuItem.render(menuField);
 });
