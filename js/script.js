6ac738f58d67b86dbd658405a9e63920a3a1639d
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-02-27 21:23:57 +0100

    added form POST

--- js/script.js
+++ js/script.js
@@ -218,4 +218,60 @@ window.addEventListener("DOMContentLoaded", () => {
    fitnesMenuItem.render(menuField);
    premiumMenuItem.render(menuField);
    postMenuItem.render(menuField);
+
+   // Forms
+
+   const forms = document.querySelectorAll("form");
+
+   const message = {
+      loading: "Loading",
+      success: "Success",
+      failure: "Failure",
+   };
+
+   forms.forEach((item) => {
+      postData(item);
+   });
+
+   function postData(form) {
+      form.addEventListener("submit", (e) => {
+         e.preventDefault();
+
+         const statusMessage = document.createElement("div");
+         statusMessage.classList.add("status");
+         statusMessage.textContent = message.loading;
+         form.append(statusMessage);
+
+         const request = new XMLHttpRequest();
+
+         request.open("POST", "server.php");
+
+         request.setRequestHeader("Content-type", "application/json");
+         const formData = new FormData(form);
+
+         const obj = {};
+         formData.forEach(function (value, key) {
+            obj[key] = value;
+         });
+
+         const json = JSON.stringify(obj);
+
+         request.send(json);
+
+         request.addEventListener("load", () => {
+            if (request.status === 200) {
+               console.log(request.response);
+               statusMessage.textContent = message.success;
+
+               form.reset();
+
+               setTimeout(() => {
+                  statusMessage.remove();
+               }, 2000);
+            } else {
+               statusMessage.textContent = message.failure;
+            }
+         });
+      });
+   }
 });
